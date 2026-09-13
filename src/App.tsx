c05162f57e811ef1eb3ed4441b7858074f9aec79
michael <mike124691@gmail.com>
2026-09-13 15:46:46 -0400

    feat(init): Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import About from './pages/About'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
